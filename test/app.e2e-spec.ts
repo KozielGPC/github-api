ff53fc34282ddd2975fd40646a8c737f02531e1c
KozielGPC <gpcgabriel0@gmail.com>
2022-07-09 00:58:53 -0300

    add test for user details

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -15,10 +15,14 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
+  it('should return user github details', () => {
+    const data = { "login": "KozielGPC", "id": 37910437, "node_id": "MDQ6VXNlcjM3OTEwNDM3", "avatar_url": "https://avatars.githubusercontent.com/u/37910437?v=4", "gravatar_id": "", "url": "https://api.github.com/users/KozielGPC", "html_url": "https://github.com/KozielGPC", "followers_url": "https://api.github.com/users/KozielGPC/followers", "following_url": "https://api.github.com/users/KozielGPC/following{/other_user}", "gists_url": "https://api.github.com/users/KozielGPC/gists{/gist_id}", "starred_url": "https://api.github.com/users/KozielGPC/starred{/owner}{/repo}", "subscriptions_url": "https://api.github.com/users/KozielGPC/subscriptions", "organizations_url": "https://api.github.com/users/KozielGPC/orgs", "repos_url": "https://api.github.com/users/KozielGPC/repos", "events_url": "https://api.github.com/users/KozielGPC/events{/privacy}", "received_events_url": "https://api.github.com/users/KozielGPC/received_events", "type": "User", "site_admin": false, "name": "Márcio Gabriel", "company": "UZUM ™ ", "blog": "", "location": "Campo Mourão - PR", "email": null, "hireable": null, "bio": null, "twitter_username": null, "public_repos": 18, "public_gists": 0, "followers": 29, "following": 37, "created_at": "2018-03-29T17:23:24Z", "updated_at": "2022-07-06T19:42:51Z" }
+    return request(app.getHttpServer()).get('/api/users/kozielgpc/details')
+      .expect(data)
+  })
+
+  it('should return status 500', () => {
+    return request(app.getHttpServer()).get('/api/users/kozielgggggpc/details')
+      .expect(500)
+  })
 });
