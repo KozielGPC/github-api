07e9172e447d493fe61099674a65104482fac4a3
KozielGPC <gpcgabriel0@gmail.com>
2022-07-09 19:36:33 -0300

    update controller spec

--- src/api/github/github.controller.spec.ts
+++ src/api/github/github.controller.spec.ts
@@ -1,12 +1,20 @@
+import { HttpModule, HttpService } from '@nestjs/axios';
 import { Test, TestingModule } from '@nestjs/testing';
 import { GithubController } from './github.controller';
+import { GithubModule } from './github.module';
+import { GithubService } from './github.service';
 
 describe('GithubController', () => {
   let controller: GithubController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule, GithubModule],
       controllers: [GithubController],
+      providers: [GithubService, {
+        provide: HttpService,
+        useValue: {}
+      }],
     }).compile();
 
     controller = module.get<GithubController>(GithubController);
